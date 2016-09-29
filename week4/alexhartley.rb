613d9bfdab3293b56c739ef6b2000fc30c6aadc8
alexhartley <alexhartley@gmail.com>
2016-09-29 22:48:18 +0100

    Add days and years

--- week4/alexhartley.rb
+++ week4/alexhartley.rb
@@ -1,6 +1,9 @@
 def calculate_hours(mins)
   hours, minutes = mins.divmod(60)
-	"#{hours} hour(s) and #{minutes} min(s)"
+  days, hours = hours.divmod(24)
+  years, days = days.divmod(365)
+
+  "#{years} year(s), #{days} day(s), #{hours} hour(s) and #{minutes} min(s)"
 end
 
 puts "hackspace timesheet program"
