314d1c7531a7c7974d6af761ce453cdd2a6d4f56
alexhartley <alexhartley@gmail.com>
2016-09-29 22:34:58 +0100

    Assign hours and minutes

--- week4/alexhartley.rb
+++ week4/alexhartley.rb
@@ -1,6 +1,6 @@
 def calculate_hours(mins)
-  hours = mins.divmod(60)
-	"#{hours[0]} hour(s) and #{hours[1]} min(s)"
+  hours, minutes = mins.divmod(60)
+	"#{hours} hour(s) and #{minutes} min(s)"
 end
 
 puts "hackspace timesheet program"
